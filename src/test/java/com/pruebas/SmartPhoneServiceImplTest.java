a0ad28769f811c5719c7d01319bd2f173ecd3551
JanireG <jgarciadi23dw@ikzubirimanteo.com>
2024-04-17 18:42:31 +0200

    Test para comprobar que el metodo count no devuelve null

--- src/test/java/com/pruebas/SmartPhoneServiceImplTest.java
+++ src/test/java/com/pruebas/SmartPhoneServiceImplTest.java
@@ -1,5 +1,15 @@
 package com.pruebas;
 
+import com.example.demo.service.*;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class SmartPhoneServiceImplTest {
 
+    @Test
+    void count() {
+        SmartPhoneServiceImpl sp = new SmartPhoneServiceImpl();
+        assertNotNull(sp.count());
+    }
 }
