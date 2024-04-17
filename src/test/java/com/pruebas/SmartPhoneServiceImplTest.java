b0c0dc048962f9c1093fc46a99a6c91d27b25dd4
JanireG <jgarciadi23dw@ikzubirimanteo.com>
2024-04-17 18:46:57 +0200

    Crear test para método findOne para verificar que se lanza la excepción

--- src/test/java/com/pruebas/SmartPhoneServiceImplTest.java
+++ src/test/java/com/pruebas/SmartPhoneServiceImplTest.java
@@ -13,7 +13,17 @@ class SmartPhoneServiceImplTest {
         // assertNotNull(sp.count());
         // assertTrue(sp.count()>0);
         // assertEquals(3, sp.count());
-        assertEquals(2, sp.count());
+
+        //assertEquals(2, sp.count());
 
     }
+
+    void findOne() {
+        SmartPhoneServiceImpl sp = new SmartPhoneServiceImpl();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            sp.findOne(null);
+        });
+        assertEquals("El ID no puede ser nulo !", exception.getMessage());
+    }
+
 }
